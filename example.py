21644e2139929f6cd1ae33f2d8929b6125cf4b7c
jmbhughes <hughes.jmb@gmail.com>
2022-10-25 10:05:31 -0600

    reworked example for presentation

--- example.py
+++ example.py
@@ -1,60 +1,56 @@
-import inspect
-from numbers import Real
-from typing import Any
-
 import numpy as np
-import matplotlib.pyplot as plt
-
-from psfpy import simple_psf, varied_psf, FunctionalCorrector, ArrayCorrector
+from psfpy import simple_psf, varied_psf, FunctionalCorrector
 from psfpy.corrector import calculate_covering
 
 
+def elliptical_gaussian2d(x, y, height, cen_x, cen_y, sigma_x, sigma_y, rotation):
+    a = np.square(np.cos(rotation)) / (2 * np.square(sigma_x)) + np.square(np.sin(rotation)) / (2 * np.square(sigma_y))
+    b = - np.sin(2 * rotation) / (4 * np.square(sigma_x)) + np.sin(2 * rotation) / (4 * np.square(sigma_y))
+    c = np.square(np.sin(rotation)) / (2 * np.square(sigma_x)) + np.square(np.cos(rotation)) / (2 * np.square(sigma_y))
+    return height * np.exp(-(a * np.square(x - cen_x) + 2 * b * (x - cen_x) * (y - cen_y) + c * np.square(y - cen_y)))
+
+
 @simple_psf
-def gaussian(x, y, x0, y0, sigma_x, sigma_y):
-    return np.exp(-(np.square(x-x0)/(2*np.square(sigma_x)) + np.square(y-y0)/(2*np.square(sigma_y))))
+def elongated_gaussian(x, y,
+                       core_height, core_x, core_y, core_sigma,
+                       tail_height, tail_x, tail_y, tail_sigma_x, tail_sigma_y, tail_rotation,
+                       background):
+    core = elliptical_gaussian2d(x, y, core_height, core_x, core_y, core_sigma, core_sigma, 0)
+    tail = elliptical_gaussian2d(x, y, tail_height, tail_x, tail_y, tail_sigma_x, tail_sigma_y, tail_rotation)
+    return core + tail + background
+
+
+@varied_psf(elongated_gaussian)
+def punch_model(x, y):
+    return {
+        "core_height": 1,
+        "core_x": 16,
+        "core_y": 16,
+        "core_sigma": (x+1)*3,
+        "tail_height": (y+1)*4,
+        "tail_x": 5 * np.cos(np.arctan2(y, x)),
+        "tail_y": 5 * np.sin(np.arctan2(y, x)),
+        "tail_sigma_x": 5,
+        "tail_sigma_y": 1,
+        "tail_rotation": np.arctan2(y, x)
+    }
 
 
 @simple_psf
 def target_model(x, y):
-    x0 = 16
-    y0 = 16
-    sigma_x = 3
-    sigma_y = 3
+    """ symmetric gaussian """
+    x0, y0, sigma_x, sigma_y = 16, 16, 3, 3
     return np.exp(-(np.square(x-x0)/(2*np.square(sigma_x)) + np.square(y-y0)/(2*np.square(sigma_y))))
 
 
-@varied_psf(gaussian)
-def my_psf(x: Real | np.ndarray, y: Real | np.ndarray) -> dict[str, Any]:
-    return {
-        "x0": 16,
-        "y0": 16,
-        "sigma_x": (x+1)*3,
-        "sigma_y": (y+1)*4
-    }
-
-
 if __name__ == "__main__":
-    show_steps = True
-
     uncorrected_image = np.zeros((1048, 1048))
 
-    my_model = FunctionalCorrector(my_psf, target_model)
-    if show_steps:
-        corners = calculate_covering(uncorrected_image.shape, 250)
-        array_corrector = my_model.evaluate_to_array_form(corners[:, 0], corners[:, 1], 250)
-        corrected_image = array_corrector.correct_image(uncorrected_image)
-    else:
-        corrected_image = my_model.correct_image(uncorrected_image, 250)
+    # use a functional model to correct an image
+    my_model = FunctionalCorrector(punch_model, target_model)
+    corrected_image = my_model.correct_image(uncorrected_image, 32)
 
-    fig, axs = plt.subplots(ncols=2, sharex=True, sharey=True)
-    axs[0].imshow(uncorrected_image)
-    axs[1].imshow(uncorrected_image)
-    plt.show()
-
-    #array_corrector = my_model.evaluate_to_array_form(np.arange(10), np.arange(10), 10)
-    # array_corrector.save("array_corrector.corr")
-    # loaded = ArrayCorrector.load("array_corrector.corr")
-    # print(type(loaded))
-    # evaluated_model = my_model.evaluate(np.arange(100), np.arange(100), 100)
-    # print(evaluated_model[0, 0])
-    # corrected_image = evaluated_model.correct_image(uncorrected_image)
+    # save an evaluated array form of the model for faster computation in the future
+    corners = calculate_covering(uncorrected_image.shape, 250)
+    array_corrector = my_model.evaluate_to_array_form(corners[:, 0], corners[:, 1], 250)
+    array_corrector.save("my_array_corrector.psfpy")
