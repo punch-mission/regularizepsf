eec5031886e2f0f5e211f9fcde4dfa11b05ff2ac
Sam Van Kooten <vankooten.sam@gmail.com>
2023-03-31 15:01:40 -0600

    Add matplotlib to requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author='J. Marcus Hughes',
     author_email='hughes.jmb@gmail.com',
     ext_modules=cythonize(ext_modules, annotate=True, compiler_directives={'language_level': 3}),
-    install_requires=["numpy", "dill", "deepdish", "lmfit", "sep", "cython", "astropy", "scipy", "scikit-image"],
+    install_requires=["numpy", "dill", "deepdish", "lmfit", "sep", "cython", "astropy", "scipy", "scikit-image", "matplotlib"],
     package_data={"regularizepsf": ["helper.pyx"]},
     setup_requires=["cython"],
     extras_require={"test": ['pytest', 'coverage', 'pytest-runner']}
