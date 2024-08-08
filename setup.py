b536e5af8d0ec1c5ad4c1ea8186c9e4459b0cdd8
Marcus Hughes <hughes.jmb@gmail.com>
2024-08-07 23:21:29 -0600

    clean up commented out, bump version

--- setup.py
+++ setup.py
@@ -2,47 +2,7 @@ import numpy as np
 from Cython.Build import cythonize
 from setuptools import setup
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
-
-# ext_modules = [Extension("regularizepsf.helper",
-#                          sources=["regularizepsf/helper.pyx"],
-#                          include_dirs=[np.get_include()])]
-
 setup(
-    # name="regularizepsf",
-    # python_requires=">=3.10",
-    # version="0.3.4",
-    # description="Point spread function modeling and regularization",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    # packages=["regularizepsf"],
-    # url="https://github.com/punch-mission/regularizepsf",
-    # license="MIT",
-    # author="J. Marcus Hughes",
-    # author_email="hughes.jmb@gmail.com",
-    # ext_modules=cythonize(ext_modules, annotate=True, compiler_directives={"language_level": 3}),
     ext_modules=cythonize("regularizepsf/helper.pyx"),
     include_dirs=[np.get_include()]
-
-    # install_requires=["numpy==1.26.4",
-    #                   "dill",
-    #                   "h5py",
-    #                   "lmfit",
-    #                   "sep",
-    #                   "cython",
-    #                   "astropy",
-    #                   "scipy",
-    #                   "scikit-image",
-    #                   "matplotlib",
-    #                   "setuptools"],
-    # package_data={"regularizepsf": ["helper.pyx"]},
-    # setup_requires=["cython"],
-    # extras_require={"test": ["pytest",
-    #                          "pytest-cov",
-    #                          "hypothesis",
-    #                          "coverage",
-    #                          "ruff",
-    #                          "pytest-mpl",
-    #                          "pre-commit"]}
 )
